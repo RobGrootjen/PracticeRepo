d6d0de3076a8927cda769c1c6963030524de7400
Rob Grootjen <robgrootjen1@gmail.com>
2021-02-01 16:39:37 -0600

    new practice

--- practice1/app.js
+++ practice1/app.js
@@ -1,19 +1,11 @@
 function askForName(){
-    let passWord = prompt('What is the password of your bank account?');
-    if (passWord === 'Lenovo123') {
-        let helpResponse = prompt('Password is right, what would you like to do? \n1. Check balance. \n2. Make a payment \n3. Transfer');
-        if (helpResponse == 1) {
-            alert('Your balance is $1230.12')
-        }
-        if (helpResponse == 2) {
-            let makePayment = prompt('How much would you like to pay?')
-        }
-        if (helpResponse == 3){
-            let whoTransfer = prompt('Who would you like to transfer to?')      
-        }
-    }else{
-        alert('Password is wrong')
-    }
+    let distance = prompt('Input the distance, and I\'ll calculate the time in minutes.');
+    var milesPH = 40;
+    var firstResult = distance / milesPH;
+    var secondResult = firstResult * 60;
+    alert('It takes ' + secondResult + ' minutes to drive ' + distance + 'miles!');
+
+
     
 }
 
