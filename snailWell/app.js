133497b402ddc863cd5cf2a087ded183670e67dc
Rob Grootjen <robgrootjen1@gmail.com>
2021-02-02 17:22:24 -0600

    snailwell practice

--- snailWell/app.js
+++ snailWell/app.js
@@ -1,1 +1,17 @@
-alert('hey!')
+function snailWell(){
+    var days = 0;
+    var miles = 0;
+    var distanceUp = 7;
+    var slipBack = 2;
+    var distance = prompt("How many miles would you like to calculate?");
+    while (miles <= distance) {
+        days++;
+        miles += distanceUp;
+        if (miles < distance) {
+            miles -= slipBack;
+        }
+        console.log(days);
+    }
+}
+
+snailWell();
