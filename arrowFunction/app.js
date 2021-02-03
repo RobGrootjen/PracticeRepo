6ed38136c3123a3564f83cdc67518877de978c07
Rob Grootjen <robgrootjen1@gmail.com>
2021-02-03 15:27:56 -0600

    new lesson

--- arrowFunction/app.js
+++ arrowFunction/app.js
@@ -7,3 +7,9 @@ console.log(displayPi());
 
 const area = r => 3.14 * r * r;
 console.log(area(10));
+
+const hello = () => console.log('Hello there!');
+hello();
+
+const hi = () => "Hi there";
+console.log(hi());
