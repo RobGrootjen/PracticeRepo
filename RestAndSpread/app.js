a6807487f50624ce2cb429c1354e7a5a273f3db4
Rob Grootjen <robgrootjen1@gmail.com>
2021-02-06 09:39:45 -0600

    new push

--- RestAndSpread/app.js
+++ RestAndSpread/app.js
@@ -12,3 +12,35 @@ function containsAll(arr){
 let x = [2,4,6,7];
 console.log(containsAll(x,2,4,7));
 console.log(containsAll(x,6,4,9));
+
+
+
+function magic(...nums){
+    let sum = 0;
+    nums.filter(n => n % 2 == 0).map(el => sum += el);
+    return sum
+}
+console.log(magic(1,2,3,4,5,6));
+
+
+
+function myFunction(w, x, y, z){
+    console.log(w + x + y + z);
+}
+var args = [1,2,3];
+myFunction.apply(null, args.concat(4));
+
+
+
+
+const myFunction2 = (w, x, y, z) => {
+    console.log(w + x + y + z);
+};
+let args2 = [1, 2, 3];
+myFunction2(...args2, 4);
+
+
+
+var dateFields = [1970, 0, 1];
+var date = new Date(...dateFields);
+console.log(date);
